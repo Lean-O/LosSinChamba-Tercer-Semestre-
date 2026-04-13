4ab11bb33735ccf93bfdd7338e83d57323d2039b
Samira <bazsamira79@gmail.com>
2026-04-13 14:03:05 -0300

    Eliminar carpeta personal del repositorio

--- Samira_Baz/Python/Clase1_Excepciones/numerosIgualesException.py
+++
@@ -1,6 +0,0 @@
-#Clases excepciones personalizadas: podemos crear excepciones aparte de las que ya están definidas
-
-class NumerosIgualesIguales (Exception): #Se extiende de la clase exception
-    def __innit__(self, mensaje):
-        self.message = mensaje
-
