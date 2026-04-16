0b87f9624fcf3521268138502c960d7cefe14656
Mariano Rasgido <mariano.rasggido@gmail.com>
2026-04-15 23:21:19 -0300

    subo archivos de la leccion uno, mundo pc

---
+++ Java/Leccion01/MundoPc/Monitor.java
@@ -0,0 +1,46 @@
+public class Monitor {
+
+    private final int idMonitor;
+    private String marca;
+    private double tamaño;
+    private static int contadorMonitores;
+
+    public Monitor(String marca, double tamaño) {
+        this.idMonitor = ++Monitor.contadorMonitores;
+        this.marca = marca;
+        this.tamaño = tamaño;
+    }
+
+    public int getIdMonitor() {
+        return this.idMonitor;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public double getTamaño() {
+        return tamaño;
+    }
+
+    public void setTamaño(double tamaño) {
+        this.tamaño = tamaño;
+    }
+
+    public static int getContadorMonitores() {
+        return contadorMonitores;
+    }
+
+    public static void setContadorMonitores(int contadorMonitores) {
+        Monitor.contadorMonitores = contadorMonitores;
+    }
+
+    @Override
+    public String toString() {
+        return "Monitor [idMonitor=" + idMonitor + ", marca=" + marca + ", tamaño=" + tamaño + "]";
+    }
+}
